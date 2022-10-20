15a2427e69a8c351436821b11de05539f43829e9
Rawmiz <rawmizthapa@gmail.com>
2022-10-19 22:59:09 -0700

    vendor registration

---
+++ vendor/forms.py
@@ -0,0 +1,8 @@
+from dataclasses import fields
+from django import forms
+from .models import Vendor
+
+class vendorForm(forms.ModelForm):
+    class Meta:
+        model = Vendor
+        fields = ['vendor_name', 'vendor_license']
