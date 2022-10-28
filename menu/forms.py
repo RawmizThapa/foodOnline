7a22864987cd86ca1c981fd05094e3dbf9f4c006
Rawmiz <rawmizthapa@gmail.com>
2022-10-28 13:15:45 -0700

    Category Crud functionalities

---
+++ menu/forms.py
@@ -0,0 +1,9 @@
+from dataclasses import field
+from django import forms
+from .models import Category
+
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ['category_name', 'description']
